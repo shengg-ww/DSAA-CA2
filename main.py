b9ffa4dfc02d68d861b0f57fe42ddea05149fcb7
shengg-ww <limsw2993@gmail.com>
2024-07-04 10:16:50 +0800

    screen

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello world')
+import turtle
+
+# Configure the screen and pen
+pen = turtle.Turtle() # is like a pen
+scr = turtle.Screen() # is like a canvas
+pen.speed( 'fastest') # 'fastest' to 'slowest'
+pen.hideturtle()   
+
+# Set window dimensions (we go for fullscreen window)
+scr.setup(width=1.0, height=1.0) 
